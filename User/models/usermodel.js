b0b072626cd43a97f1c7bd5e1b7b0378080a003a
tsizion <tsionephrem7@gmail.com>
2024-11-08 17:19:13 +0300

    username random

--- User/models/usermodel.js
+++ User/models/usermodel.js
@@ -25,15 +25,22 @@ const UserSchema = new mongoose.Schema(
       type: [String], // Array of addresses
       default: [],
     },
-
     status: {
       type: String,
       enum: ["Active", "Inactive"],
       default: "Active",
     },
+    username: {
+      type: String,
+      unique: true, // Ensure username is unique
+      default: function () {
+        return "User" + Math.floor(100000 + Math.random() * 900000); // Generate random 6-digit username
+      },
+    },
   },
   { timestamps: true }
 );
+
 UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     return next();
@@ -51,6 +58,7 @@ UserSchema.methods.createResetPasswordToken = function () {
   const resetToken = crypto.randomBytes(32).toString("hex");
   return crypto.createHash("sha256").update(resetToken).digest("hex");
 };
+
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
